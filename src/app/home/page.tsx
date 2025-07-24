05c2faadd77a3dea4a22e89afc307ba625a4ca55
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-07-24 17:48:04 +0900

    feat : 카카오 맵 띄우기 성공 디자인 수정 필요

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,19 @@
+import {CardDemo} from '@/components/custom/map/map'
+import {Map} from 'react-kakao-maps-sdk'
+const mayKey = process.env.KAKAO_MAP_KEY
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-full h-full bg-primaryC">
-      <span>Hello World</span>
-      {/* . */}
+    <div className="flex items-center justify-center w-full h-full">
+      <div id="map" style={{width: '1000px', height: '600px'}}>
+        <script
+          type="text/javascript"
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${mayKey}`}></script>
+        <Map
+          center={{lat: 33.450701, lng: 126.570667}}
+          style={{width: '1000px', height: '600px'}}
+          level={3}
+        />
+      </div>
     </div>
   )
 }
