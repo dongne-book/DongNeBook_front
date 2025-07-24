923755bd1d4491de749e66777ffe8eec9640076e
JihyunKim <kimwlgus001130@gmail.com>
2025-07-24 15:42:43 +0900

    First Commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-full h-full bg-primaryC">
+      <span>Hello World</span>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primaryC: '#0070f3'
+      }
+    }
+  },
+  plugins: []
+}
