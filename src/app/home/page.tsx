cdfcb313408543d624d7d342ae2e6d6836d25210
JihyunKim <kimwlgus001130@gmail.com>
2025-07-24 15:52:18 +0900

    폴더 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center w-full h-full bg-primaryC">
       <span>Hello World</span>
+      {/* . */}
     </div>
   )
 }
