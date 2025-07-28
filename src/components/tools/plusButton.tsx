ebd77cfd3ff31993e67d05eb490a84a23e51e8d5
JihyunKim <kimwlgus001130@gmail.com>
2025-07-28 16:11:44 +0900

    fix: 플러스 버튼 내부 메뉴 마진 추가

--- src/components/tools/plusButton.tsx
+++ src/components/tools/plusButton.tsx
@@ -14,7 +14,7 @@ export default function PlusButton() {
       <DropdownMenuTrigger className="fixed z-50 flex items-center justify-center w-12 h-12 rounded-full bottom-20 right-5 bg-primaryC">
         <Plus className="w-8 h-8 text-white" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent className="mr-4">
         <DropdownMenuItem>한 컷 올리기</DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem>오늘을 기록하기</DropdownMenuItem>
